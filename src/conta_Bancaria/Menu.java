001e5279e5ce4fd3793820d63a7ca47014b0a588
jackeline <jackelinepessoa0@gmail.com>
2026-04-29 23:57:34 -0300

    atualizações conta bancaria e menus de cores adicionado.

--- src/conta_Bancaria/Menu.java
+++ src/conta_Bancaria/Menu.java
@@ -2,7 +2,11 @@ package conta_Bancaria;
 
 import java.util.Scanner;
 
+import conta_Bancaria.util.Cores;
+
 public class Menu {
+
+	
 
 	public static void main(String[] args) {
 
@@ -10,85 +14,83 @@ public class Menu {
 		int opcao;
 
 		while (true) {
-			System.out.println("###########################################################");
-			System.out.println("                                                           ");
-			System.out.println("                  BANCO BRADESCO COM S                     ");
-			System.out.println("                                                           ");
-			System.out.println("###########################################################");
-			System.out.println("                                                           ");
-			System.out.println("             1- Criar Conta                                ");
-			System.out.println("             2- Listar todas as Contas                     ");
-			System.out.println("             3- Buscar Conta por Numero                    ");
-			System.out.println("             4- Atualizar Dados da Conta                   ");
-			System.out.println("             5- Apagar Conta                               ");
-			System.out.println("             6- Sacar                                      ");
-			System.out.println("             7 - Depositar                                 ");
-			System.out.println("             8 - Tranferir valores entre Contas            ");
-			System.out.println("             0- Sair                                       ");
-			System.out.println("                                                           ");
-			System.out.println("###########################################################");
-			System.out.println(" Entre com a opção desejada:                               ");
-			System.out.println("                                                           ");
+			
+
+			System.out.println(Cores.TEXT_YELLOW + Cores.ANSI_BLACK_BACKGROUND
+					+ "*****************************************************");
+			System.out.println("                                                     ");
+			System.out.println("                BANCO Bradesco COM S               ");
+			System.out.println("                                                     ");
+			System.out.println("*****************************************************");
+			System.out.println("                                                     ");
+			System.out.println("            1 - Criar Conta                          ");
+			System.out.println("            2 - Listar todas as Contas               ");
+			System.out.println("            3 - Buscar Conta por Numero              ");
+			System.out.println("            4 - Atualizar Dados da Conta             ");
+			System.out.println("            5 - Apagar Conta                         ");
+			System.out.println("            6 - Sacar                                ");
+			System.out.println("            7 - Depositar                            ");
+			System.out.println("            8 - Transferir valores entre Contas      ");
+			System.out.println("            0 - Sair                                 ");
+			System.out.println("                                                     ");
+			System.out.println("*****************************************************");
+			System.out.println("Entre com a opção desejada:                          ");
+			System.out.println("                                                     " + Cores.TEXT_RESET);
 
 			opcao = leia.nextInt();
 
 			if (opcao == 0) {
-				System.out.println("\nBanco Bradesco com S- O seu futuro começa aqui! ");
+				System.out.println(Cores.TEXT_WHITE_BOLD + "\nBanco Bradesco com  S - O seu Futuro começa aqui!");
 				sobre();
 				leia.close();
 				System.exit(0);
 			}
+
 			switch (opcao) {
-
 			case 1:
-				System.out.println("Criar Conta\n\n");
-				break;
+				System.out.println(Cores.TEXT_WHITE + "Criar Conta\n\n");
 
+				break;
 			case 2:
-				System.out.println("Listar todas as Contas\n\n");
-				break;
+				System.out.println(Cores.TEXT_WHITE + "Listar todas as Contas\n\n");
 
+				break;
 			case 3:
-				System.out.println("Consultar dados da Conta - por número\n\n");
-				break;
+				System.out.println(Cores.TEXT_WHITE + "Consultar dados da Conta - por número\n\n");
 
+				break;
 			case 4:
-				System.out.println("Atualizar dados da Conta\n\n");
-				break;
+				System.out.println(Cores.TEXT_WHITE + "Atualizar dados da Conta\n\n");
 
+				break;
 			case 5:
-				System.out.println("Apagar a Conta\n\n");
-				break;
+				System.out.println(Cores.TEXT_WHITE + "Apagar a Conta\n\n");
 
+				break;
 			case 6:
-				System.out.println("Saque\n\n");
-				break;
+				System.out.println(Cores.TEXT_WHITE + "Saque\n\n");
 
+				break;
 			case 7:
-				System.out.println("Depósito\n\n");
-				break;
+				System.out.println(Cores.TEXT_WHITE + "Depósito\n\n");
 
+				break;
 			case 8:
-				System.out.println("Transferência entre Contas\n\n");
-				break;
+				System.out.println(Cores.TEXT_WHITE + "Transferência entre Contas\n\n");
 
+				break;
 			default:
-				System.out.println("\nOpção Inválida!\n");
+				System.out.println(Cores.TEXT_RED_BOLD + "\nOpção Inválida!\n" + Cores.TEXT_RESET);
 				break;
 			}
 		}
 	}
 
-	private static void sobre() {
-		
-		    System.out.println("\n*****************************************************");
-		    System.out.println("Projeto Desenvolvido por: Jackeline Pessoa Gomes  ");
-		    System.out.println("Generation Brasil - jackeinepessoa0@gmail.com");
-		    System.out.println("https://github.com/jackeline5458/ProjetoJava-assincrono");
-		    System.out.println("*****************************************************");
-		}
-		
-
+	public static void sobre() {
+		System.out.println("\n*********************************************************");
+		System.out.println("Projeto Desenvolvido por:Jackeline Pessoa Gomes  ");
+		System.out.println("Generation Brasil -jackelinepessoa0@gmail.com");
+		System.out.println("https://github.com/jackeline5458/conta_Bancaria");
+		System.out.println("*********************************************************");
 	}
-
-
+}
