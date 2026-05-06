76141bed343d9bd04176a7c0ca37983b0beb4ec7
jackeline <jackelinepessoa0@gmail.com>
2026-05-05 23:27:52 -0300

    adicionando conta corrente e poupança

--- src/conta_Bancaria/Menu.java
+++ src/conta_Bancaria/Menu.java
@@ -2,7 +2,10 @@ package conta_Bancaria;
 
 
 import java.util.Scanner;
+
 import conta_Bancaria.model.Conta;
+import conta_Bancaria.model.ContaCorrente;
+import conta_Bancaria.model.ContaPoupanca;
 import conta_Bancaria.util.Cores;
 
 public class Menu {
@@ -20,6 +23,21 @@ public class Menu {
 
 	        c1.sacar(12000.0f);
 			c1.visualizar();
+			
+			ContaCorrente cc1 = new ContaCorrente(2, 123, 1, "José da Silva", 0.0f, 1000.0f);
+			cc1.visualizar();
+			cc1.sacar(12000.0f);
+			cc1.visualizar();
+			cc1.depositar(5000.0f);
+			cc1.visualizar();
+					
+			
+			ContaPoupanca cp1 = new ContaPoupanca(3, 123, 2, "Maria dos Santos", 100000.0f, 15);
+			cp1.visualizar();
+			cp1.sacar(1000.0f);
+			cp1.visualizar();
+			cp1.depositar(5000.0f);
+			cp1.visualizar();
 			
 			
 		Scanner leia = new Scanner(System.in);
